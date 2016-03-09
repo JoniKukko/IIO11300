ae47922541e53a173bfe62a871f1fdf3e5256ffd
Joni Kukko <joni.kukko@outlook.com>
2016-03-09 11:33:32 +0200

    Tehtävä 7 aloitettu

---
+++ IIO11300Vktehtavat/Tehtava7/MainWindow.xaml.cs
@@ -0,0 +1,41 @@
+﻿using System.Windows;
+using System.Windows.Controls;
+
+namespace Tehtava7
+{
+
+    public partial class MainWindow : Window
+    {
+        private int 
+            symbolCountInt = 0, 
+            bigLetterCountInt = 0, 
+            smallLetterCountInt = 0, 
+            numberCountInt = 0, 
+            specialSymbolCountInt = 0;
+
+
+        public MainWindow()
+        {
+            InitializeComponent();
+            this.updateLabels();
+        }
+
+
+
+        private void password_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            this.symbolCountInt = password.Text.Length;
+        }
+
+
+
+        private void updateLabels()
+        {
+            this.symbolCount.Content = this.symbolCountInt;
+            this.bigLetterCount.Content = this.bigLetterCountInt;
+            this.smallLetterCount.Content = this.smallLetterCountInt;
+            this.numberCount.Content = this.numberCountInt;
+            this.specialSymbolCount.Content = this.specialSymbolCountInt;
+        }
+    }
+}
